f0e2ac4a3fa2f05fed0e94109e8ba8367f9cd27e
Shun Takamura <shun@ShunnoMacBook-Air.local>
2025-10-26 21:04:12 -0700

    WIP

---
+++ app/src/clean-architecture/domain/entities/book.ts
@@ -0,0 +1,24 @@
+export class Book {
+  constructor(
+    private _id: string,
+    private _title: string,
+    private _isAvailable: boolean = true,
+    private _createdAt: Date = new Date(),
+    private _updatedAt: Date = new Date()
+  ) {}
+
+  // TypeScriptではgetterは`get`で定義する
+  get id(): string            { return this._id}
+  get title(): string         { return this._title }
+  get isAvailable(): boolean  { return this._isAvailable }
+  get createdAt(): Date       { return this._createdAt }
+  get updatedAt(): Date       { return this._updatedAt }
+
+  loan() {
+    if (this.isAvailable) {
+      this._isAvailable = false;
+    } else {
+      throw new Error('この本は既に貸し出し中です')
+    }
+  }
+}
